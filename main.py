77fa0d3223583459ddbedaf302a222267b09d283
JathurshanG <jathur93@gmail.com>
2020-12-09 11:17:15 +0100

    Fonction permettant la lecture du texte

--- main.py
+++ main.py
@@ -3,3 +3,10 @@
 ##l'objectif ici est de traduire du scale en python ###########################
 
 
+import sys
+from pyspark import SparkContext, SparkConf
+
+    words = sc.textFile("D:/workspace/spark/input.txt").flatMap(lambda line: line.split(" "))
+
+    # count the occurrence of each word
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
