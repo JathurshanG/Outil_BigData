d85761c96e3e4cf3162de183241e311809c7b4fd
JathurshanG <jathur93@gmail.com>
2020-12-10 20:21:59 +0100

    Projet TP1 , traduction Scala

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 ####Date de Rendu :  11/12/2020 ###############################################
 ##l'objectif ici est de traduire du scale en python ###########################
 
+#Initalisation de spark, au préalable télécharger findSparks avec pip
 
-import sys
+import findspark 
+findspark.init("C:\Spark")
+
+#entrypoint des API Spark
 from pyspark import SparkContext, SparkConf
 
-    words = sc.textFile("D:/workspace/spark/input.txt").flatMap(lambda line: line.split(" "))
+conf =SparkConf().setAppName("wordCount").setMaster("local")
+sc = SparkContext(conf=conf)
 
-    # count the occurrence of each word
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+word=sc.textFile("sample.txt")
+#exportation des données choisit soit sample.txt
+count=word.flatMap(lambda line: line.split(" "))
+#dans cette étape on considère comme donnée chaque mots, l'éspace étant
+count = count.map(lambda word: (word, 1))
+#Creation d'un tuple permettant de compte chaque mots
+count=count.reduceByKey(lambda a,b:a +b)
+#on associe a chaque le nombre de fois où apparaît le mots
+print(count.collect)
+#afficher les mots
