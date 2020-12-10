328697fd6210992990cf78b83b616a77c5d64b55
JathurshanG <71498491+JathurshanG@users.noreply.github.com>
2020-12-11 00:22:00 +0100

    Update main.py
    Entête plus joli

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-##########################TP 1 Outil Big DATA #################################
-####Date de Rendu :  11/12/2020 ###############################################
-##l'objectif ici est de traduire du scale en python ###########################
+# =============================================================================
+# Matière : Outil Big Data
+# Date de rendu : 12/12/2020
+# TP concernant Le word Count
+# Groupe 15
+# =============================================================================
 
-#Initalisation de spark, au préalable télécharger findSparks avec pip
+import findspark
 
-import findspark 
 findspark.init("C:\Spark")
 
 #entrypoint des API Spark
@@ -14,12 +16,14 @@ conf =SparkConf().setAppName("wordCount").setMaster("local")
 sc = SparkContext(conf=conf)
 
 word=sc.textFile("sample.txt")
-#exportation des données choisit soit sample.txt
+print(word.collect())
+
 count=word.flatMap(lambda line: line.split(" "))
 #dans cette étape on considère comme donnée chaque mots, l'éspace étant
 count = count.map(lambda word: (word, 1))
 #Creation d'un tuple permettant de compte chaque mots
 count=count.reduceByKey(lambda a,b:a +b)
 #on associe a chaque le nombre de fois où apparaît le mots
-print(count.collect)
-#afficher les mots
+
+for x in count.collect():
+    print(x)
